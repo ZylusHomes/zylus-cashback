8663fa4b0cfff1d5c75f1c790d7914d2157b8452
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-12-21 15:57:05 +0100

    update 12

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
+import logo from "@/public/smallLogo.png";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,21 +18,11 @@ export const metadata: Metadata = {
   title: "Real Estate Cashback Cooperative",
   description:
     "Empowering your financial Capabilities Through Smart Investments",
-  // keywords: [],
   authors: [
     {
       name: "Ayeni Emmanuel",
-      // url: ""
     },
   ],
-  // icons: {
-  //   icon: "../public/smallLogo.png",
-  // },
-  icons: {
-    icon: "../public/smallLogo.png", // Relative to the `public` directory
-    shortcut: "../public/smallLogo.png",
-    apple: "../public/smallLogo.png",
-  },
 };
 
 export default function RootLayout({
@@ -42,9 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" type="image/png" href="../public/smallLogo.png" />
-      </Head>
+      <link rel="icon" href={logo.src} sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
